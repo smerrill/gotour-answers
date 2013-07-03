609403bb7926d77d648a64f91ca4aa6488f8c7e4
Steven Merrill <steven.merrill@gmail.com>
2013-07-03 19:12:10 -0400

    Better answer w/ loop count.

--- problem23.go
+++ problem23.go
@@ -5,12 +5,14 @@ import (
 	"math"
 )
 
-func Sqrt(x float64) float64 {
-	z := x
-	for i := 0; i < 10; i++ {
-		z = z - ((math.Pow(z, 2) - x) / (2 * z))
+func Sqrt(x float64) (float64, int) {
+	y, z, count := 1.0, x, 1
+	for y >= 0.000001 {
+		y = ((math.Pow(z, 2) - x) / (2 * z))
+		z = z - y
+		count++
 	}
-	return z
+	return z, count
 }
 
 func main() {
